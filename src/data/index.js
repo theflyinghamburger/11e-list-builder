b50a5b03bc97bbc09a1b13cf88c174a69fd89c43
Soham Roy <soham.roy.msit@gmail.com>
2026-06-28 10:19:05 +0000

    Added all factions

--- src/data/index.js
+++ src/data/index.js
@@ -10,6 +10,23 @@ import tauEmpire from './tau-empire.json';
 import thousandSons from './thousand-sons.json';
 import tyranids from './tyranids.json';
 import worldEaters from './world-eaters.json';
+import adeptaSororitas from './adepta-sororitas.json';
+import adeptusCustodes from './adeptus-custodes.json';
+import aeldari from './aeldari.json';
+import astraMilitarum from './astra-militarum.json';
+import blackTemplars from './black-templars.json';
+import chaosDaemons from './chaos-daemons.json';
+import chaosTitanLegions from './chaos-titan-legions.json';
+import chaosSpaceMarines from './chaos-space-marines.json';
+import chaosKnights from './chaos-knights.json';
+import deathwatch from './deathwatch.json';
+import emperorsChildren from './emperors-children.json';
+import imperialAgents from './imperial-agents.json';
+import genestealerCults from './genestealer-cults.json';
+import greyKnights from './grey-knights.json';
+import leaguesOfVotann from './leagues-of-votann.json';
+import spaceWolves from './space-wolves.json';
+import titanLegions from './titan-legions.json';
 
 const STORAGE_KEY = 'custom-factions';
 
@@ -26,6 +43,23 @@ const factions = {
   'thousand-sons': thousandSons,
   'tyranids': tyranids,
   'world-eaters': worldEaters,
+  'adepta-sororitas': adeptaSororitas,
+  'adeptus-custodes': adeptusCustodes,
+  'aeldari': aeldari,
+  'astra-militarum': astraMilitarum,
+  'black-templars': blackTemplars,
+  'chaos-daemons': chaosDaemons,
+  'chaos-titan-legions': chaosTitanLegions,
+  'chaos-space-marines': chaosSpaceMarines,
+  'chaos-knights': chaosKnights,
+  'deathwatch': deathwatch,
+  'emperors-children': emperorsChildren,
+  'imperial-agents': imperialAgents,
+  'genestealer-cults': genestealerCults,
+  'grey-knights': greyKnights,
+  'leagues-of-votann': leaguesOfVotann,
+  'space-wolves': spaceWolves,
+  'titan-legions': titanLegions,
 };
 
 try {
